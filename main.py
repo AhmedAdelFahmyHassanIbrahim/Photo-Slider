03dc38b2bbf2259aa132c3d73a046f658b47ebf3
Ahmed Adel Fahmy <ahmed.hedia@outlook.com>
2020-11-13 16:23:19 +0200

    Update main.py
    adding the main features and functions in the code to display the images

--- main.py
+++ main.py
@@ -6,12 +6,48 @@ class App(tk.Tk):
     docstring
     """
     def __init__(self, image_files, x, y, delay) -> None:
+
         tk.Tk.__init__(self)
-        self.geometry('+ {}+ {}'.format(x,y))
+
+        self.geometry('+{}+{}'.format(x,y))
         self.delay = delay
+
         self.pictures = cycle((tk.PhotoImage(file = image), image)
         for image in image_files)
 
         self.pictures_display = tk.Label(self)
-        self.picture_display.pack()
-        
+        self.pictures_display.pack()
+
+    def show_slides(self):
+        """
+        docstring
+        """
+        img_object, img_name = next(self.pictures)
+        self.picture_display.config(image = img_object)
+        self.title(img_name)
+        self.after(self.delay, self.show_slides)
+
+    def run(self):
+        """
+        docstring
+        """
+        self.mainloop()
+
+delay= 3500
+
+image_files = [
+    '1.gif',
+    '2.gif', 
+    '3.gif', 
+    '4.gif', 
+    '5.gif', 
+    '6.gif',
+    '7.gif'
+    ]
+
+x = 100
+y = 50
+
+app = App(image_files, x,y, delay)    
+app.show_slides()
+app.run()
